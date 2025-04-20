afccab836dfdf18a293ba28759b791d1c435e814
SHAIK46371 <ayubshaik0187@gmail.com>
2025-04-20 06:12:50 +0000

    Added terraform configuration files

---
+++ EC2-INSTANCE/variable.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  description = "AWS region to launch resources"
+  type        = string
+}
+
+variable "ami" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
